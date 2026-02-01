a89786aee542fcf2937f8e8fccd7f2fe9d73b7a2
Will Bush <git@willbush.dev>
2026-02-01 01:50:35 -0600

    add nix flake with dev shell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "My blog";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            zola
+            wrangler
+          ];
+        };
+      }
+    );
+}
